c956390990469a079b82855b4a906548d29cb57b
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2022-06-27 22:28:36 -0300

    last studi about useState react

--- discover/src/views/loja/Home.jsx
+++ discover/src/views/loja/Home.jsx
@@ -1,6 +1,6 @@
 import './style.css'
 import { Button } from "react-bootstrap"
-
+import {useState} from "react"
 
 import {Card} from "../../components/card/"
 
@@ -8,7 +8,28 @@ import {Card} from "../../components/card/"
 
 function Home() {
 
+  //estado react
+  //[ 1ª valor do estado "valor que eu quero utilizar")
+  //  2ª função que atualiza o valor do estado
+  // 3ª =...() seria o valor inicial do estado
   
+
+  // [nomeclatura nomrmal , setnomeclatura ]
+  const [name1,setName] = useState("")
+  const [cardName, setCardName] = useState([])
+
+  function testeState(){
+    const value = {
+      name:name1,
+      time: new Date().toLocaleDateString("pt-br",{
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      })
+    }
+
+    setCardName(preveState =>[...preveState,value])
+  }
 
   return (
 
@@ -16,13 +37,23 @@ function Home() {
       <h1 className="title">Hello world</h1>
 
       <div className="container2">
-        <input id='a' type="text" placeholder="Digite seu nome..." />
-        <Button variant="primary">Primary</Button>
+        <input 
+        id='a' 
+        type="text" 
+        placeholder="Digite seu nome..."
+        onChange={e => setName(e.target.value)}/>
+
+        <Button 
+        variant="primary"
+        onClick={testeState}
+        >Primary</Button>
         
       </div>
 
-      <Card name ="Igor" time="10:55:25"/>
-      <Card name ="João" time="11:55:25"/>
+      {
+        cardName.map(texto => <Card name ={texto.name} time={texto.time}/>)
+      }
+      
 
     </div>
   )
