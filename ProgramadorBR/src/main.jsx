6f38d5bd5891b55b287c616d263f2aee98fc2898
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2022-07-01 18:46:01 -0300

    Correction a problem of version react

--- ProgramadorBR/src/main.jsx
+++ ProgramadorBR/src/main.jsx
@@ -5,8 +5,4 @@ import App2 from "./App2"
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App2 />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
